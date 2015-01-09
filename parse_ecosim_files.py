8ce79957508a5dfebc4c27787a99d7376ef26318
Luis Miguel Morillas <morillas@gmail.com>
2015-01-09 10:54:48 +0100

    parsing

--- parse_ecosim_files.py
+++ parse_ecosim_files.py
@@ -9,8 +9,16 @@ BASE = 'http://academics.smcvt.edu/dmccabe/teaching/Community/'
 
 doc = html.parse('http://academics.smcvt.edu/dmccabe/teaching/Community/NullModelData.html')
 ecosim_files = doc.xml_select(u'//a[contains(@href, "matrices_ecosim")]')
+notes = doc.xml_select(u'//a[contains(@href, "matrices_notes")]')
+
 
 example = ecosim_files[100]
+ex_name = U(example).lower()
+#### Review
+for n in notes:
+    if ex _name in U(n).lower():
+        break
+
 
 url = urlparse.urljoin(BASE, example.href)
 
