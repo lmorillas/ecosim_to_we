b07ebc660828ac4cd139cfceee62d719f153d152
Luis Miguel Morillas <morillas@gmail.com>
2015-01-10 13:24:07 +0100

    ready for testing

---
+++ create_we_pages.py
@@ -0,0 +1,72 @@
+import mwclient
+
+USER = ''
+PWD = ''
+
+# where it is going to create the tree
+WEBASE = "User:Lmorillas/Sandbox/NullModelData"
+
+# we connection
+site = mwclient.Site('wikieducator.org', path="/")
+site.login(USER, PWD)
+
+# create index with a table
+def create_index(data):
+    text_index = """{{MyTitle| Null Model Data}}
+
+==Files for null model analysis==
+
+{{note|intended for educational use}}
+
+These matrices were compiled under NSF grant BSR 9106981 awarded to Bruce Patterson in 1991.  The grant supported a collaboration of Wirt Atmar, Alan Cutler, Gregory Mikkelson, Bruce Patterson, and David Wright.  The original matrices, some additional matrices, and notes concerning their derivation can be downloaded in full from AICS Research INC.  Data sets are unzipped with the software.
+
+'''Recommended citation format''': ''Atmar, J.W., and B.D. Patterson. 1995. The nestedness temperature calculator: a visual basic program, including 294 presence-absence matrices. AICS Research, Inc., University Park, NM, and The Field Museum, Chicago.''
+
+'''Disclaimer''':  In the files linked on the left below, I have removed the notes and citations, and transposed the data. While I have not intentionally modified the data in any other way, neither have I gone back to the original publications to confirm accuracy.
+
+'''Easy save option''': Right click on any link below.  Select 'save target as', and save it to a location of your choice.  The resulting file can be opened from Ecosim.
+
+{| border="1" class="wikitable"
+|+ Files for null model analysis
+! Title
+! Sites
+! Species
+"""
+
+    initial_page_end = """
+    |}
+
+    [[Category:NullModeData]]
+    """
+
+    table = [new_element(*d) for d in data]
+
+
+    initial_page = site.Pages[WEBASE]
+    initial_page.save(text_index + ''.join(table) + initial_page_end, summary='Creating null mode data with mwclient')
+
+
+def new_element(title, site, species):
+    return '''|-
+    ! {}
+    | {}
+    | {}
+    '''.format('[[{}/{}|{}]]'.format(WEBASE, title, title), site, species)
+
+
+
+TEMPLATE = open('template_note.txt').read()
+def new_page_content(d):
+    '''
+    d --> dictionary: title, content, data
+    '''
+    newp = TEMPLATE.format(**d)
+    newp = newp.replace('}', '}}')
+    newp = newp.replace('{', '{{')
+    newp = newp.replace('\n ', '\n')
+    return newp
+
+def create_page(d):
+    title = d.get('title').strip()
+    newpage = site.Pages[WEBASE + '/' + title]
+    newpage.save(new_page_content(d), summary='Creating null mode data page for' + title)
--- parse_ecosim_files.py
+++ parse_ecosim_files.py
@@ -3,9 +3,8 @@ from amara.lib import U
 import urllib
 import urlparse
 import shelve
+from create_we_pages import *
 
-
-fnotes = shelve.open('notes.dat')
 
 BASE = 'http://academics.smcvt.edu/dmccabe/teaching/Community/'
 
@@ -24,6 +23,7 @@ def parse_notes_file(f):
             content.append(line)
     return {'title': title, 'content': ' '.join(content)}
 
+
 def parse_anchor_ecosim(anchor):
     name = U(anchor).lower().strip()
     url = urlparse.urljoin(BASE, anchor.href)
@@ -36,11 +36,34 @@ def parse_anchor_notes(anchor):
 
 
 
-def parse_ecosim_file(f):
+def parse_ecosim_file(url):
+
+    '''returns data, species, files'''
+
     f = urllib.urlopen(url)
     lines = [l for l in f.readlines()]
     species = len(lines) -1
     sites = len(lines[0].split()) -1
+    return ''.join(lines), species, sites
+
+
+def create_shelve(notesdict):
+    '''
+    Create a notes.dat shelve file. For testing
+    key:
+    value: dict: title / content of the notes file
+    '''
+    fnotes = shelve.open('notes.dat')
+    for k in notesdict:
+        print k
+        fnotes[str(k)] = parse_notes_file(urllib.urlopen(notesdict.get(k)))
+
+    #typo
+    fnotes['1000islm.txt'] = fnotes.pop('notes and \n\t\t\t\t\t\tdata from:1000islm.txt')
+    fnotes.sync()
+    fnotes.close()
+
+
 
 
 
@@ -51,9 +74,21 @@ if __name__ == '__main__':
     notes_files = doc.xml_select(u'//a[contains(@href, "matrices_notes")]')
     ecodict = dict([parse_anchor_ecosim(e) for e in ecosim_files])
     notesdict = dict([parse_anchor_notes(e) for e in notes_files])
-    for k in notesdict:
-        print k
-        fnotes[str(k)] = parse_notes_file(urllib.urlopen(notesdict.get(k)))
+    ecokeys = ecodict.keys()
+    ecokeys.sort()
+    #create_shelve(notesdict)  # is the shelve created?
+    fnotes = shelve.open('notes.dat')
+    pages = []
 
-    fnotes.sync()
-    fnotes.close()
+    for x in range(10):
+        k = str(ecokeys[x])  # element to process
+        data, species, sites = parse_ecosim_file(ecodict.get(k))
+        d = fnotes.get(k)
+        if not d:
+            print 'no encontrado', k
+            break
+        d['data'] = data
+        create_page(d)
+        pages.append([d.get('title'), species, sites])
+
+    create_index(pages)
