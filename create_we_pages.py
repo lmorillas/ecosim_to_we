04fbdf57a24b3d99e955e113f775ba1a03256533
Luis Miguel Morillas <morillas@gmail.com>
2015-01-30 15:05:20 +0100

    Refactoring and documenting

--- create_we_pages.py
+++ create_we_pages.py
@@ -1,4 +1,8 @@
 import mwclient
+from mwclient.errors import APIError
+
+
+#comfigure bot
 
 USER = ''
 PWD = ''
@@ -6,9 +10,12 @@ PWD = ''
 # where it is going to create the tree
 WEBASE = "NullModelData"
 
-# we connection
+TEMPLATE = open('template_note.txt').read()
+
+# configure mediawiki connection
 site = mwclient.Site('wikieducator.org', path="/")
 site.login(USER, PWD)
+
 
 # create index with a table
 def create_index(data):
@@ -31,49 +38,104 @@ These matrices were compiled under NSF grant BSR 9106981 awarded to Bruce Patter
 ! Title
 ! Sites
 ! Species
+! Data source
 """
-
     initial_page_end = """
     |}
 
     [[Category:NullModeData]]
     """
+    datos = []
+    for d in data:
+        x = {}
+        x['title'] = d['title']
+        x['site'] = d['sites']
+        x['species'] = d['species']
+        x['link'] = d['source']
+        datos.append(x)
 
-    table = [new_element(*d) for d in data]
-
+    table = [new_element(**d) for d in datos]
 
     initial_page = site.Pages[WEBASE]
-    initial_page.save(text_index + ''.join(table) + initial_page_end, summary='Creating null mode data with mwclient')
+    content = text_index + ''.join(table) + initial_page_end
+    initial_page.save(content, summary='Creating null mode data with mwclient')
 
 
-def new_element(title, site, species):
+def new_element(title, site, species, link):
+    name = link.split('/')[-1].lower()
     return '''|-
-    ! {}
     | {}
     | {}
-    '''.format('[[{}/{}|{}]]'.format(WEBASE, title, title), site, species)
+    | {}
+    | {}
+    '''.format('[[{}/{}|{}]]'.format(WEBASE, title, title), site, species,
+            '[{}  {}]'.format(link, name))
 
 
-
-TEMPLATE = open('template_note.txt').read()
 def new_page_content(d):
     '''
-    d --> dictionary: title, content, data
+    d --> dictionary: title, content, data, source, name
     '''
     newp = TEMPLATE.format(**d)
-#    newp = newp.replace('}', '}}')
-#    newp = newp.replace('{', '{{')
     newp = newp.replace('\n ', '\n')
     return newp
 
-def create_page(d, src=None):
-    '''
 
-    '''
-    title = d.get('title').strip()
+def create_page_from_template(data):
+    d = {}
+    title = data.get('title').strip()
+    d['title'] = title
     title = WEBASE + '/' + title
-    d['urlsrc'] = src
-    d['namesrc'] = src.split('/')[-1].strip().lower()
+    d['title'] = title
+    d['content'] = data['content'].encode('utf-8')
+    d['data'] = data['data'].encode('utf-8')
+    src = data.get('source')
+    d['source'] = src  # can't write links now :-(
+    d['name'] = src.split('/')[-1].strip().lower()
 
-    newpage = site.Pages[title]
-    newpage.save(new_page_content(d), summary='Creating null mode data page for' + title)
+    print 'creating', title
+    content = new_page_content(d)
+    return {'title':title, 'content':content}
+
+def create_we_pages(d):
+    '''
+    This need a bot account because pages have external links
+
+    d --> dict: title / content keys
+    '''
+    title = d.get('title')
+    d.get('content')
+
+    try:
+        newpage = site.Pages[title]
+        newpage.save(d.get('content'), summary='Creating null mode data page for' + title)
+    except APIError, e:
+        print e
+        errors.append(title)
+    except:
+        print 'Error with', title
+        errors.append(title)
+
+
+if __name__ == '__main__':
+
+    import json
+    import time
+
+    errors = []
+
+    # requires page_to_create.json
+    pages = json.load(open('pages_to_create.json'))
+
+    we_pages = []
+
+    for p in pages:
+        we_pages.append(create_page_from_template(p))
+
+    map(create_we_pages, we_pages)
+
+    # review errors list
+
+    pages.sort(key=lambda x: x['title'])
+
+    content = create_index(pages)
